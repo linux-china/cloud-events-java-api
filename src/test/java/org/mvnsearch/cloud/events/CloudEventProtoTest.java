a3f02b8737e034e9035593db0be4c37bcca887bf
linux_china <linux_china@hotmail.com>
2018-05-29 11:48:33 -0700

    cloud event proto test

---
+++ src/test/java/org/mvnsearch/cloud/events/CloudEventProtoTest.java
@@ -0,0 +1,29 @@
+package org.mvnsearch.cloud.events;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+
+/**
+ * cloud event proto test
+ *
+ * @author linux_china
+ */
+public class CloudEventProtoTest {
+    @Test
+    public void testProtoOperation() throws Exception {
+        CloudEventProtos.CloudEventProto event = CloudEventProtos.CloudEventProto.newBuilder()
+                .setCloudEventsVersion("0.1")
+                .setContentType("text/plain")
+                .putExtensions("name", "linux_china")
+                .setEventTime(System.currentTimeMillis())
+                .build();
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        event.writeTo(output);
+        CloudEventProtos.CloudEventProto event2 = CloudEventProtos.CloudEventProto.parseFrom(output.toByteArray());
+        Assert.assertEquals(event2.getContentType(), "text/plain");
+        Assert.assertEquals(event2.getExtensionsMap().get("name"), "linux_china");
+    }
+}
